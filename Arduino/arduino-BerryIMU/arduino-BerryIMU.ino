c535345891e8a4a4cc6c08d537c672778e4a3921
Cynopolis <Cynopolis@users.noreply.github.com>
2019-10-02 00:03:17 -0500

    Added an angle function

--- Arduino/arduino-BerryIMU/arduino-BerryIMU.ino
+++ Arduino/arduino-BerryIMU/arduino-BerryIMU.ino
@@ -1,3 +1,5 @@
+#include <AutoPID.h>
+
 #include "IMU.h"
 
 #define DT  0.02          // Loop time
@@ -10,29 +12,26 @@
  * Compass data is in IMUData[2] (raw compass data)
  */
 float IMUData[3][3];
-
+//the calculated heading from the compass
 float heading = 0;
+
+//0.5 to 2.5 or 3 ms on-time PWM signal
+AutoPID
 
 void setup() {
   Serial.begin(9600);
-
+  //begins I2C communications with the IMU
   detectIMU();
   enableIMU();
+  //read the data from the IMU
   readIMU();
 }
 
 void loop() {
   readIMU();
-  Serial.print("X: ");
-  Serial.print(IMUData[2][0]);
-  Serial.print(", Y: ");
-  Serial.print(IMUData[2][1]);
-  Serial.print(", Z: ");
-  Serial.print(IMUData[2][2]);
-  Serial.print(", Heading: ");
-  Serial.println(heading);
 }
 
+//read the data from the IMU
 void readIMU(){ 
   byte buff[6];
   unsigned int startTime = millis();
@@ -73,4 +72,14 @@ void readIMU(){
         {
             delay(1);
         }
+}
+
+//calculate the polar angles for the accelerometer
+float[] calcLevelAngle(){
+  float angles[3];
+  float magnitude = sqrt(sq(IMUData[0][0]) + sq(IMUData[0][1]) + sq(IMUData[0][2]));
+  for(int i = 0; i < 3; i++){
+    angles[i] = acos(IMUData[0][i]/magnitude);
+  }
+  return angles;
 }
